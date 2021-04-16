7d68778908e22382280217bce4e13a72b088aff5
Ahmad Rulim <mfalkia342@gmail.com>
2021-04-16 14:38:52 +0700

    Update hostname
    Signed-off-by: Ahmad Rulim <mfalkia342@gmail.com>

--- app/src/main/kotlin/com/kweeksnews/lite/ApplicationClass.kt
+++ app/src/main/kotlin/com/kweeksnews/lite/ApplicationClass.kt
@@ -3,7 +3,7 @@ package com.kweeksnews.lite
 import android.app.Application
 import com.onesignal.OneSignal
 
-const val ONESIGNAL_APP_ID = "f97432b8-bdfd-4ceb-90f3-82ddeec78212"
+const val ONESIGNAL_APP_ID = "7ec3d423-9f1c-4d2f-bff6-cb9b5e6a0edc"
 
 class ApplicationClass : Application() {
     override fun onCreate() {
